935e482f0077f21b5b142c665720fb0b556ad82e
FredrikAndreasson <fredrik.andreasson@yandex.com>
2020-11-16 13:27:09 +0100

    Cleaner game1

--- MarioPlatformer/MarioPlatformer/Game1.cs
+++ MarioPlatformer/MarioPlatformer/Game1.cs
@@ -7,7 +7,7 @@ namespace MarioPlatformer
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private SpriteBatch SpriteBatch;
 
         public Game1()
         {
@@ -16,18 +16,10 @@ namespace MarioPlatformer
             IsMouseVisible = true;
         }
 
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-        }
-
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,18 +27,12 @@ namespace MarioPlatformer
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
-
-            base.Draw(gameTime);
         }
     }
 }
